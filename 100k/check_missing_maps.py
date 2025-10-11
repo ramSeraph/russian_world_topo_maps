0dcde9dbaaba09d9a5dd1c83c0495c29f6da9bd1
ramSeraph <kandimalla.sreeram@gmail.com>
2025-10-11 23:33:24 +0530

    dump the 100k code

--- 100k/check_missing_maps.py
+++ 100k/check_missing_maps.py
@@ -1,5 +1,6 @@
 
 import json
+from pathlib import Path
 
 def check_missing_maps():
     try:
@@ -16,11 +17,20 @@ def check_missing_maps():
         print("Error: mapstor/data/sheet_map.json not found.")
         return
 
+    mapstor_bad_sheets = Path('mapstor/bad_sheets.txt').read_text().splitlines()
+    mapstor_bad_sheets = [line.strip() for line in mapstor_bad_sheets if line.strip()]
+    mapstor_bad_sheets = set([line.replace('.gif', '') for line in mapstor_bad_sheets])
+
+    all_vlasenko_map = {}
     all_vlasenko_keys = []
     for k in vlasenko_data.keys():
         all_vlasenko_keys.extend(k.split('_'))
+        for p in k.split('_'):
+            all_vlasenko_map[p] = k
     all_mapstor_keys = []
     for k in mapstor_data.keys():
+        if k in mapstor_bad_sheets:
+            continue
         all_mapstor_keys.extend(k.split('_'))
     vlasenko_keys = set(all_vlasenko_keys)
     mapstor_keys = set(all_mapstor_keys)
@@ -33,9 +43,10 @@ def check_missing_maps():
 
     print("Keys in vlasenko/data/sheet_map.json but not in mapstor/data/sheet_map.json:")
     for key in sorted(list(missing_keys)):
-        map_info = vlasenko_data.get(key, {})
+        sheet = all_vlasenko_map[key]
+        map_info = vlasenko_data.get(sheet, {})
         map_exists = map_info.get('map_exists', 'N/A')
-        print(f"- {key}: map_exists is {map_exists}")
+        print(f"- {sheet}: map_exists is {map_exists}")
 
 if __name__ == "__main__":
     check_missing_maps()
